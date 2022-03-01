36891e9b4e0b6ec0d8f722aa7686fe5b6b83ba35
Michael Berger <michael@bergerbytes.io>
2022-03-01 10:35:34 -0600

    Target swift tools 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,11 +7,10 @@ let package = Package(
     name: "StateKit",
     platforms: [
         .iOS(.v13),
-        .macOS(.v11),
+        .macOS(.v10_15),
         .tvOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "StateKit",
             targets: ["StateKit"]),
@@ -24,11 +23,10 @@ let package = Package(
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "StateKit",
-            dependencies: ["Debug"]),
+            dependencies: ["Debug"]
+        ),
         .testTarget(
             name: "StateKitTests",
             dependencies: ["StateKit"]),
