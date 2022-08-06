d11d1aeea660678764ebbb2ad7732c5bf8ee60f6
Michael Berger <michael@bergerbytes.io>
2022-08-06 18:30:11 -0500

    Remove NSObservableStore

--- Sources/StateKit/UI/iOS/SwiftUI/ViewWith.swift
+++ Sources/StateKit/UI/iOS/SwiftUI/ViewWith.swift
@@ -24,27 +24,3 @@ public struct ViewWith<State: StateContainer, Store: ObservableViewStore<State>,
         view(store)
     }
 }
-
-@available(iOS 14.0, macOS 11.0, *)
-public extension ViewWith {
-    init(_ store: Store, view: @escaping (Store) -> Content) {
-        self.init(store: store, view: view)
-    }
-}
-
-@available(iOS 14.0, macOS 11.0, *)
-public struct NSViewWith<State: StateContainer, Store: NSObservableViewStore<State>, Content>: View where Content: View {
-    @StateObject var store: Store
-    @ViewBuilder var view: (Store) -> Content
-
-    public var body: some View {
-        view(store)
-    }
-}
-
-@available(iOS 14.0, macOS 11.0, *)
-public extension NSViewWith {
-    init(_ store: Store, view: @escaping (Store) -> Content) {
-        self.init(store: store, view: view)
-    }
-}
